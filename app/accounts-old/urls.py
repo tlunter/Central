84cf38692d698974147bd7f1df91948dcfd2c1bd
Todd Lunter <tlunter@gmail.com>
2012-05-26 16:57:13 -0400

    Accounts old isn't needed anymore

--- app/accounts-old/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import patterns, include, url
-
-urlpatterns = patterns('app.accounts.views',
-	url(r'^profile/(?P<username>[^/]+)/$', 'profile', name = 'account-profile'),
-	url(r'^profile/$', 'profile', name = 'account-profile'),
-	url(r'^logout/$', 'logout', name = 'account-logout'),
-	url(r'^login/$', 'login', name = 'account-login')
-)
